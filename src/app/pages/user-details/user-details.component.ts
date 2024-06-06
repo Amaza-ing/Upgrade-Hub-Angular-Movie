332c98652c54577a11d8b6fd77d6c22fe0bfa43e
Adrián Maza <adri_mv1@hotmail.com>
2024-06-06 20:00:26 +0200

    user details added

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import User from '../../models/User';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css',
 })
 export class UserDetailsComponent implements OnInit {
-  constructor(public userService: UserService, private route: ActivatedRoute) {}
+  selectedUser?: any;
+
+  constructor(private route: ActivatedRoute, public userService: UserService) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
-    this.getUserFromComponent(id);
+    this.getUser(id);
   }
 
-  getUserFromComponent(id: number): void {
+  getUser(id: number) {
     this.userService.getUser(id).subscribe({
       next: (data) => {
         console.log(data);
+        this.selectedUser = data;
       },
       error: (e) => {
         console.log(e);
--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
 })
